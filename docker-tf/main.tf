060bc14c20b9dc0fbb5e65f672b4350513a440c3
Kamal Kaliyaperumal <kamal.sk@gmail.com>
2024-02-12 05:17:00 +0000

    terraform and docker

---
+++ docker-tf/main.tf
@@ -0,0 +1,27 @@
+#terraform block
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.22.0"
+    }
+  }
+}
+#provider block
+provider "docker" {}
+
+#resource block
+resource "docker_image" "nginx" {
+  name         = "nginx:1.19.6"
+  keep_locally = true    // keep image after "destroy"
+}
+
+#resource block
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 2224
+  }
+}
